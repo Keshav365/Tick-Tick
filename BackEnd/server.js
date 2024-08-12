217e2c1888e42b41180fb678b583465bd5ff3346
Keshav365 <67777hdh@gmail.com>
2024-08-12 11:47:43 +0530

    something big

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -15,7 +15,7 @@ import linkRoutes from './routes/links.js';
 app.use(express.json());
 
 const corsOptions = {
-    origin: ['http://localhost:5173','http://localhost:5174'], // Allow multiple origins
+    origin: process.env.CORS_ORIGIN, // Allow multiple origins
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 };
 
